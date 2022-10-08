2536f857b10e58b620f2923bbcb1ea72b5775ccb
Khurshid <umaribnxattob8008@gmail.com>
2022-10-08 22:10:22 +0900

    [add::] PropertyAccessors

---
+++ src/main/kotlin/PropertyAccessors/PropertyAccessors4.kt
@@ -0,0 +1,12 @@
+package PropertyAccessors
+
+class Cage2 (private val maxCapacity: Int){
+    private val hamsters =
+        mutableListOf<Hamster>()
+    fun capacity(): Int =
+    maxCapacity - hamsters.size
+    fun isFull(): Boolean =
+        hamsters.size == maxCapacity
+
+
+}
